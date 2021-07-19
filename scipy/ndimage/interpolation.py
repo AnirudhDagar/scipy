ecc50d06dc8024be96e14071cf3d84c4e426f810
Anirudh Dagar <anirudhdagar6@gmail.com>
2021-07-20 02:47:14 +0530

    MAINT: Use uarray multimethods for ndimage.<module>.func call

---
+++ scipy/ndimage/interpolation.py
@@ -0,0 +1,16 @@
+from . import _multimethods
+
+__all__ = ['spline_filter1d', 'spline_filter', 'geometric_transform',
+           'map_coordinates', 'affine_transform', 'shift', 'zoom', 'rotate']
+
+
+def __dir__():
+    return __all__
+
+
+def __getattr__(name):
+    if name not in __all__:
+        raise AttributeError(
+            f"scipy.ndimage.interpolation has no attribute {name}.")
+
+    return getattr(_multimethods, name)
