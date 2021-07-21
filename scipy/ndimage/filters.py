8cf80ca6ca305ba118360407461d8a62766de1f4
Anirudh Dagar <anirudhdagar6@gmail.com>
2021-07-21 18:21:37 +0530

    DEP: Deprecate ndimage public looking private modules

--- scipy/ndimage/filters.py
+++ scipy/ndimage/filters.py
@@ -1,3 +1,8 @@
+# This file is not meant for public use and will be removed in the future
+# versions of SciPy. Use the `scipy.ndimage` namespace for importing the
+# functions included below.
+
+import warnings
 from . import _multimethods
 
 __all__ = ['correlate1d', 'convolve1d', 'gaussian_filter1d', 'gaussian_filter',
@@ -18,5 +23,9 @@ def __getattr__(name):
     if name not in __all__:
         raise AttributeError(
             f"scipy.ndimage.filters has no attribute {name}.")
+
+    warnings.warn(f"Please use `{name}` from the `scipy.ndimage` namespace, "
+                  "the `scipy.ndimage.filters` namespace is deprecated.",
+                  category=DeprecationWarning, stacklevel=2)
 
     return getattr(_multimethods, name)
--- scipy/ndimage/interpolation.py
+++ scipy/ndimage/interpolation.py
@@ -1,3 +1,8 @@
+# This file is not meant for public use and will be removed in the future
+# versions of SciPy. Use the `scipy.ndimage` namespace for importing the
+# functions included below.
+
+import warnings
 from . import _multimethods
 
 __all__ = ['spline_filter1d', 'spline_filter', 'geometric_transform',
@@ -12,5 +17,9 @@ def __getattr__(name):
     if name not in __all__:
         raise AttributeError(
             f"scipy.ndimage.interpolation has no attribute {name}.")
+
+    warnings.warn(f"Please use `{name}` from the `scipy.ndimage` namespace, "
+                  "the `scipy.ndimage.interpolation` namespace is deprecated.",
+                  category=DeprecationWarning, stacklevel=2)
 
     return getattr(_multimethods, name)
